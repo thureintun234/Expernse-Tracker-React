c0c66200fd0364894d34553c1f2d2e5a34309a71
thureintun234 <thurein12456@gmail.com>
2020-09-29 16:13:43 +0630

    Updates

---
+++ precache-manifest.df6c3b8d7436881d72de05aa39fffe01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "833b3af609e62c7eb9c04db650b26b2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e9fefac4482b6c8705e",
+    "url": "/static/css/main.f65cf5c3.chunk.css"
+  },
+  {
+    "revision": "2b891812e6be6cf78c58",
+    "url": "/static/js/2.56b38b7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56b38b7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e9fefac4482b6c8705e",
+    "url": "/static/js/main.783d50fc.chunk.js"
+  },
+  {
+    "revision": "fd1e764c12408bbbbfc1",
+    "url": "/static/js/runtime-main.f859b388.js"
+  }
+]);
